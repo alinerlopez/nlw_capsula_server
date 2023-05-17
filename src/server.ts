9308afee8a4a2e05b2c93adb651d376c5cf1f967
Aline <alopez@marinformatica.com.br>
2023-05-17 13:18:47 -0300

    Preparação ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const app = fastify()
+const prisma = new PrismaClient()
 // metodos http: get para  listar - post para criar algo - put atualizar algo
 // patch para atualizar algo especifico - delete para deletar
 // 32 minutos de aula
-app.get('/hello', () => {
-  return 'Hello world'
+
+app.get('/user', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('👌 HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('👌 HTTP server running on http://localhost:3333')
+  })
